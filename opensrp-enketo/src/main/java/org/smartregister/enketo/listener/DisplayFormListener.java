df444992b2fd2859aa5dafbdc0730d7d4e3f91f1
Ephraim Muhia <emuhia@ona.io>
2017-10-18 18:09:57 +0300

    Setting up enketo
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

---
+++ opensrp-enketo/src/main/java/org/smartregister/enketo/listener/DisplayFormListener.java
@@ -0,0 +1,16 @@
+package org.smartregister.enketo.listener;
+
+import org.json.JSONObject;
+
+/**
+ * Created by keyman on 18/10/2017.
+ */
+public interface DisplayFormListener {
+
+    void saveFormSubmission(String formSubmision, String id, String formName, JSONObject
+            fieldOverrides);
+
+
+    void savePartialFormData(String formData, String id, String formName, JSONObject
+            fieldOverrides);
+}
