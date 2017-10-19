1fccc2cc0ba19968ec462f8eaeb63bb12db4baec
Ephraim Muhia <emuhia@ona.io>
2017-10-19 12:13:10 +0300

    Setup sample (In progress)
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- sample/src/main/java/org/smartregister/enketo/sample/MainActivity.java
+++ sample/src/main/java/org/smartregister/enketo/sample/MainActivity.java
@@ -3,13 +3,36 @@ package org.smartregister.enketo.sample;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+import org.smartregister.enketo.adapter.pager.EnketoRegisterPagerAdapter;
+import org.smartregister.enketo.listener.DisplayFormListener;
+import org.smartregister.enketo.view.fragment.DisplayFormFragment;
+import org.smartregister.enketo.view.fragment.RegisterFragment;
+import org.smartregister.enketo.view.viewpager.EnketoViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+
+public class MainActivity extends AppCompatActivity implements DisplayFormListener {
+
+    @Bind(R.id.view_pager)
+    EnketoViewPager mPager;
+    private FragmentPagerAdapter mPagerAdapter;
+    private int currentPage;
+
+    private String[] formNames = new String[]{};
+    private android.support.v4.app.Fragment mBaseFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +41,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        formNames = this.buildFormNameList();
+        mBaseFragment = new RegisterFragment();
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPagerAdapter = new EnketoRegisterPagerAdapter(getSupportFragmentManager(), formNames, mBaseFragment);
+        mPager.setOffscreenPageLimit(formNames.length);
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
             }
         });
     }
@@ -49,4 +87,54 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void saveFormSubmission(String formSubmision, String id, String formName, JSONObject fieldOverrides) {
+        Toast.makeText(this, formName + " submitted", Toast.LENGTH_SHORT).show();
+        switchToBaseFragment(formSubmision); 
+
+    }
+
+    @Override
+    public void savePartialFormData(String formData, String id, String formName, JSONObject fieldOverrides) {
+        Toast.makeText(this, formName + " partially submitted", Toast.LENGTH_SHORT).show();
+    }
+
+    private String[] buildFormNameList() {
+        List<String> formNames = new ArrayList<String>();
+        formNames.add("new_household_registration");
+        return formNames.toArray(new String[formNames.size()]);
+    }
+
+    private void switchToBaseFragment(final String data) {
+        final int prevPageIndex = currentPage;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mPager.setCurrentItem(0, false);
+                RegisterFragment registerFragment = (RegisterFragment) findFragmentByPosition(0);
+
+                //hack reset the form
+                DisplayFormFragment displayFormFragment = getDisplayFormFragmentAtIndex(prevPageIndex);
+                if (displayFormFragment != null) {
+                    displayFormFragment.hideTranslucentProgressDialog();
+                    displayFormFragment.setFormData(null);
+                }
+
+                displayFormFragment.setRecordId(null);
+            }
+        });
+
+    }
+
+    private android.support.v4.app.Fragment findFragmentByPosition(int position) {
+        FragmentPagerAdapter fragmentPagerAdapter = mPagerAdapter;
+        return getSupportFragmentManager().findFragmentByTag("android:switcher:" + mPager.getId() + ":" + fragmentPagerAdapter.getItemId(position));
+    }
+
+    private DisplayFormFragment getDisplayFormFragmentAtIndex(int index) {
+        return (DisplayFormFragment) findFragmentByPosition(index);
+    }
+
+
 }
